23a5f08e1b6da043e26ac52a617d84d29ca7c5a9
Jasenko Hadziomeragic <jasenko.hadziomeragic@de.bosch.com>
2021-08-23 17:01:47 +0200

    Run chrome in headless

--- src/test/java/org/olxscrapper/common/BrowserFactory.java
+++ src/test/java/org/olxscrapper/common/BrowserFactory.java
@@ -5,6 +5,7 @@ import io.github.bonigarcia.wdm.DriverManagerType;
 import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -13,7 +14,9 @@ class BrowserFactory {
         WebDriver driver;
         if (browserName != null && browserName.equals("chrome")) {
             ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.setHeadless(true);
+            driver = new ChromeDriver(options);
         } else {
             FirefoxDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
             final FirefoxOptions options = new FirefoxOptions();
