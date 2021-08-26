4824bdeba69b6cbac90a46ff0c5ed4b4e9353fda
Jasenko Hadziomeragic <hjasenko@hotmail.com>
2021-08-26 14:12:13 +0200

    Update run_olx_scrapper.sh

--- run_olx_scrapper.sh
+++ run_olx_scrapper.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 source $HOME/.bash_profile
+
+echo "Pull latest changes"
+git fetch
+git pull origin/main
+
+echo "Generate new jar file"
 mvn clean package
 
 kill -9 $(lsof -t -i:8888)
 echo "Killed process running on port 8888"
 
-java -jar target/olx-scrapper-1.0-SNAPSHOT.jar
+echo "Starting application..."
+nohup java -jar target/olx-scrapper-1.0-SNAPSHOT.jar &
